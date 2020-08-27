e87aeb726d2ae2fed8cac0a337237df135baa82a
zs <zs>
2020-08-27 18:54:24 +0800

    feat:build.sh

---
+++ build.sh
@@ -0,0 +1,1 @@
+cp ztun bin/ && cp config.db bin/ && cp resizeApi.png bin/ && ./packapp/packapp -assets ./bin/ -bin ztun -icon resizeApi.png  -identifier com.github.zzpu -name WebSocket隧道 -dmg ztun -o  ~/Documents/
